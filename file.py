7e72e562b8cd1e6071608e0f7e05156056151834
abhay <abhaygank@gmail.com>
2024-07-20 18:13:35 +0530

    file addded in branch

--- file.py
+++ file.py
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 8d2dcb6 (committed)
 print("Hello")
 print("added")
 =======
@@ -9,7 +12,10 @@ print("Hello")
 =======
 print("Hello")
 >>>>>>> c50d508 (First commit)
+<<<<<<< HEAD
 =======
 print("Hello")
 print("added")
 >>>>>>> 3001229 (Stashing)
+=======
+>>>>>>> 8d2dcb6 (committed)
